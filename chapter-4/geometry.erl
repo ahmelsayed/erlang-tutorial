d004c05b026b9560f755987e48bda22981756ea5
Ahmed ElSayed <ahmels@microsoft.com>
2015-08-01 15:19:31 -0700

    Chapter 4: Problem 1

--- chapter-4/geometry.erl
+++ chapter-4/geometry.erl
@@ -8,4 +8,8 @@ test() ->
 
 area({rectangle, Width, Height}) -> Width * Height;
 area({square, Side}) -> Side * Side;
-area({circle, Radius}) -> 3.14159 * Radius * Radius.
+area({circle, Radius}) -> 3.14159 * Radius * Radius;
+area({triangle, Height, Base}) -> 0.5 * Height * Base;
+area({triangle, A, B, C}) ->
+    S = (A + B + C)/2,
+    math:sqrt(S * (S - A) * (S - B) * (S - C)).
