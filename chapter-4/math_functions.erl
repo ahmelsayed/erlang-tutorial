cf2c5d2a0af82fe6cd9208b910f71fdad2376d48
Ahmed ElSayed <ahmels@microsoft.com>
2015-08-01 16:14:02 -0700

    Chapter 4: Problems 5, 6 and 7

--- chapter-4/math_functions.erl
+++ chapter-4/math_functions.erl
@@ -1,5 +1,6 @@
 -module(math_functions).
--export([odd/1, even/1]).
+-export([odd/1, even/1, filter/2, split/1, split1/1]).
+-import(lists, [reverse/1]).
 
 odd(X) ->
     case (X rem 2) of
@@ -8,3 +9,22 @@ odd(X) ->
     end.
 
 even(X) -> not odd(X).
+
+filter(F, L) ->
+    [X || X <- L, F(X)].
+
+split(L) ->
+    Odds = filter(fun(X) -> odd(X) end, L),
+    Evens = filter(fun(X) -> even(X) end, L),
+    {Evens, Odds}.
+
+split1(L) ->
+    split1_acc(L, [], []).
+
+split1_acc([H|T], Evens, Odds) ->
+    case even(H) of
+        true -> split1_acc(T, [H|Evens], Odds);
+        false -> split1_acc(T, Evens, [H|Odds])
+    end;
+split1_acc([], Evens, Odds) ->
+    {reverse(Evens), reverse(Odds)}.
